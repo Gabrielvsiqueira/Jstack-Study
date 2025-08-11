356987e374194a0d24fc80cc423aabf51b09694f
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-08-11 19:46:51 -0300

    feat: add update and delete routes

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -4,6 +4,8 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
