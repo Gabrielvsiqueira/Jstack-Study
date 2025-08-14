b53ec34e5005ca8742215c81b160259abba43649
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-08-14 15:13:42 -0300

    feat: add schema SQL and database connection to PostgreSQL and Node.JS

--- mycontacts/src/database/index.js
+++ mycontacts/src/database/index.js
@@ -1,10 +1,33 @@
-const { Client} = require('pg');
+// src/database/index.js
+
+const { Client } = require('pg');
 
 const client = new Client({
   host: 'localhost',
-  port: 5432,
+  port: 5435,
+  user: 'root',
   password: 'root',
   database: 'mycontacts',
-})
+});
 
 client.connect();
+
+async function Query(query) {
+  const { rows } = await client.query(query);
+  return rows;
+}
+
+Query('SELECT * FROM contacts').then(console.log);
+
+// async function testConnection() {
+//   try {
+//     await client.connect();
+//     console.log('✅ Database connected successfully!');
+
+//   } catch (error) {
+//     console.error('❌ Failed to connect to the database:', error);
+//     await client.end();
+//   }
+// }
+
+// testConnection();
